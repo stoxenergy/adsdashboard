20bc3ec6204c271c29c71004765cd519b83e0447
stoxenergy <44210035+stoxenergy@users.noreply.github.com>
2021-04-02 14:39:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function loadSportsWomen() {
 	fetch(projectUrl + '/sportsWomen')
 	.then((response) => response.json())
 	.then(json => {
-		this.sportsWomen = json.sportswomen.sort((a, b) => {
+		this.sportsWomen = json.sportsWomen.sort((a, b) => {
 			return a.votes < b.votes;
 		})
 		showAllSportsWomen();
